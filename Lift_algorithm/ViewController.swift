061f37401f6d0935fd9d911859da9cacf6aa17fb
Victoria Kravets <victoriakravets93@gmail.com>
2018-05-09 20:09:05 +0300

    Initial Commit

---
+++ Lift_algorithm/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lift_algorithm
+//
+//  Created by Victoria Kravets on 09.05.2018.
+//  Copyright © 2018 Victoria Kravets. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Lift_algorithmTests/Lift_algorithmTests.swift
@@ -0,0 +1,36 @@
+//
+//  Lift_algorithmTests.swift
+//  Lift_algorithmTests
+//
+//  Created by Victoria Kravets on 09.05.2018.
+//  Copyright © 2018 Victoria Kravets. All rights reserved.
+//
+
+import XCTest
+@testable import Lift_algorithm
+
+class Lift_algorithmTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
